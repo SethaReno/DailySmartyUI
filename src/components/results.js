3bd8859768985986d30f5435d32f0bf8207e7546
SethaReno <51383197+SethaReno@users.noreply.github.com>
2020-02-17 11:06:23 -0700

    Setup results component

---
+++ src/components/results.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Results extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Results</h1>
+        <Logo/>
+      </div>
+    );
+  }
+}
+export default Results;
